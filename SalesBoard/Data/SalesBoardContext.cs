613c5171220cf340e055f3894994bf85de36ad4e
Alex <coolazn818@hotmail.com>
2020-09-02 22:23:41 +0930

    Added database functionality with items

---
+++ SalesBoard/Data/SalesBoardContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesBoard.Models;
+
+namespace SalesBoard.Data
+
+
+{
+    public class SalesBoardContext : DbContext
+    {
+        public SalesBoardContext(DbContextOptions<SalesBoardContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Items> Items { get; set; }
+    }
+    }
--- SalesBoard/Startup.cs
+++ SalesBoard/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 namespace SalesBoard
 {
     public class Startup
@@ -28,12 +29,14 @@ namespace SalesBoard
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddDbContext<SalesBoardContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SalesBoardContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
